21eb87822ed287e388085eb025b6a1403233ecaa
Nathaniel LaPier <Nat@Nathaniels-Air.home>
2016-09-23 14:23:51 -0400

    adding basic About component with bio paragraph

---
+++ Components/About.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+
+	render() {
+		//prop const here
+
+		return (
+			<div>
+				<h1>About Me</h1>
+
+				<p>
+					Born and raised in Austin, TX, I studied economics at Trinity University before moving to New York City in 2010.   I am currently studying full stack web development at Rutgers Coding Bootcamp,  with the goal of working as a junior developer when I graduate in October.  I pay my bills working as a handyman on Taskrabbit, and a courier on UberRUSH. When I&#8217;m not coding or working, I like to explore, travel, volunteer, ride my bicycle and motorcycle, and plot to import breakfast tacos and the word &#34;y&#8217;all&#34; to NYC.
+				</p>
+
+				<p>
+					In addition to HTML, CSS and vanilla JavaScript, I can hold my own using:
+				</p>
+
+				<ul>
+					<li>jQuery</li>
+					<li>Node.js</li>
+					<li>Express.js</li>
+					<li>Firebase</li>
+					<li>MySQL</li>
+					<li>MongoDB</li>
+					<li>React.js</li>
+				</ul>
+				
+			</div>
+		);
+	}
+}
+
+export default ActivePage;
+
+/*
+
+<img src="./assets/images/Headshot2.jpg" alt="Nathaniel LaPier" class="img-circle img-responsive">
+
+
+*/
+
--- routes.jsx
+++ routes.jsx
@@ -1,14 +1,21 @@
+//===React Dependencies===
 import React from 'react';
 
 import { Route } from 'react-router';
 
+//===Import Components===
+
 //Base component, including nav bar
 import Application from './Components/Application';
 
+//About section
+import About from './Components/Application';
+
+//Portfolio section
 import Portfolio from './Components/Portfolio';
 
+//Contact page
 import Contact from './Components/Contact';
-
 
 export default (
 	<Route component={Application}>
