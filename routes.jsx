be97265a7c8b3043ba883e1e4625b0277e09afce
Nathaniel LaPier <Nat@Nathaniels-Air.home>
2016-09-23 13:34:30 -0400

    adding index.html

--- routes.jsx
+++ routes.jsx
@@ -5,11 +5,15 @@ import { Route } from 'react-router';
 //Base component, including nav bar
 import Application from './Components/Application';
 
+import Portfolio from './Components/Portfolio';
+
+import Contact from './Components/Contact';
 
 
 export default (
 	<Route component={Application}>
-		<Route path="/" component={IndexPage} />
-
+		<Route path="/" component={About} />
+		<Route path="Portfolio" component={Portfolio} />
+		<Route path="Contact" component={Contact} />
 	</Route>
 );
